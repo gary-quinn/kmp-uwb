a042840a043716e449ffd22b56a7c4992036b093
Gary Quinn <gary@atruedev.com>
2026-04-25 19:16:20 +0000

    update Package.swift for v0.2.2

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "KmpUwb",
-            url: "https://github.com/gary-quinn/kmp-uwb/releases/download/v0.2.1/KmpUwb.xcframework.zip",
-            checksum: "3c7c9f8b45cc3fa6b205ed88b0485e82da36f83d0f8436fa8c7c3ef80007fceb"
+            url: "https://github.com/gary-quinn/kmp-uwb/releases/download/v0.2.2/KmpUwb.xcframework.zip",
+            checksum: "b397fbbbd8053aff4e6861fd4ea58ee8460cc837fc1e4dc088d94c7c379fcff0"
         ),
     ]
 )
